4b38eafcff4ccf7b9f7fa356f6dc18a662f3f73a
Meghana087 <meghanadoradla@gmail.com>
2025-08-30 21:15:59 +0530

    settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'ecommerce.urls'
@@ -127,10 +128,12 @@ USE_TZ = True
 STATIC_URL = 'static/'
 # STATIC_ROOT = BASE_DIR / 'staticfiles' 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-STATICFILES_DIRS=[
-    BASE_DIR/'static'
-]
+# STATICFILES_DIRS=[
+#     BASE_DIR/'static'
+# ]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
