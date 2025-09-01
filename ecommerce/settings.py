1e4b7077ab8dc36759e72db07097e5d09df598c7
Meghana087 <meghanadoradla@gmail.com>
2025-09-01 19:47:17 +0530

    settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-w-339%u)_=yl3lff9c*t2i3h@p5^s(#j&#t@n8dpqqkk3aui08
 DEBUG = True
 
 # ALLOWED_HOSTS = ['primestore-ecommerce-production-acb5.up.railway.app']
-ALLOWED_HOSTS=['*']
+# ALLOWED_HOSTS=['*']
+ALLOWED_HOSTS=['primestore-ecommerce.up.railway.app']
 
 # Application definition
 
